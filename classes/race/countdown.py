aecb8eca327c30f103247cc589adc2048c961959
Tim <r3g3n3r@gmail.com>
2015-04-17 10:25:09 +0200

    * Countdown-Layout überarbeitet

--- classes/race/countdown.py
+++ classes/race/countdown.py
@@ -2,6 +2,7 @@
 # !/usr/bin/python
 
 import pygame
+from pygame import Surface
 import time
 
 from classes.label import Label
@@ -19,15 +20,15 @@ class Countdown():
         self.running = False
         self.actions = actions
         self.value = 3
-        self.label = Label(str(self.value), self.font, (255, 255, 255), (68, 68, 68))
-        self.label.set_position(
-            self.screen_width / 2 - self.label.width / 2,
-            self.screen_height / 2 - self.label.height / 2
-        )
+        self.font_color = (68, 68, 68)
+        self.background_color = (255, 255, 255)
+        #self.label = Label(str(self.value), self.font, (255, 255, 255), (68, 68, 68))
+        self.label = self.create_countdown()
 
     def start(self):
         self.value = 3
-        self.label.set_text(str(self.value))
+        #self.label.set_text(str(self.value))
+        self.label = self.create_countdown()
         self.start_time = time.time()
         self.running = True
 
@@ -42,10 +43,27 @@ class Countdown():
                 self.actions['success']()
             elif passed_time > 2:
                 self.value = 1
-                self.label.set_text(str(self.value))
+                self.label = self.create_countdown()
             elif passed_time > 1:
                 self.value = 2
-                self.label.set_text(str(self.value))
+                self.label = self.create_countdown()
 
     def render(self, deltat):
-        self.screen.blit(self.label.label, self.label.position)
+        self.screen.blit(self.label, (0, 0))
+
+    def create_countdown(self):
+        background = Surface((self.screen_width, self.screen_height))
+        background.fill(self.background_color)
+        countdown_value = self.font.render(str(self.value), 1, self.font_color)
+        background.blit(
+            countdown_value,
+            (self.screen_width / 2 - countdown_value.get_rect().width / 2,
+             self.screen_height / 2 - countdown_value.get_rect().height / 2)
+        )
+        return background
+
+    def invert_colors(self):
+        new_bg_color = self.font_color
+        self.font_color = self.background_color
+        self.background_color = new_bg_color
+        self.label = self.create_countdown()
